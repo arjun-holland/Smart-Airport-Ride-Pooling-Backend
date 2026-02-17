9bd4982469c834024620f6fa13474bd07e96883b
Mallikarjuna <spiderman05116@gmail.com>
2026-02-17 14:13:27 +0530

    first commit

--- src/controllers/pool.controller.ts
+++ src/controllers/pool.controller.ts
@@ -14,38 +14,3 @@ export const getAllPools = async (req: Request, res: Response) => {
 };
 
 
-
-
-// import { Request, Response } from "express";
-// import { RidePool } from "../models/ridePool.model";
-// import { Cab } from "../models/cab.model";
-
-// export const createTestPool = async (req: Request, res: Response) => {
-//   try {
-//     // Find one available cab
-//     const cab = await Cab.findOne({ isAvailable: true });
-
-//     if (!cab) {
-//       return res.status(400).json({ message: "No available cab found" });
-//     }
-
-//     const pool = await RidePool.create({
-//       cabId: cab._id,
-//       members: [],
-//       totalSeatsUsed: 0,
-//       totalLuggageUsed: 0,
-//       estimatedRouteDistanceKm: 0,
-//     });
-
-//     // Mark cab unavailable
-//     cab.isAvailable = false;
-//     cab.currentPoolId = pool._id as any;
-//     await cab.save();
-
-//     res.status(201).json(pool);
-    
-//   } catch (error: any) {
-//     console.error(error);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
